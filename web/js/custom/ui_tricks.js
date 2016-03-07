654f9fc109089b893be6c3e07394a17f216fc944
Memoona <mem.mughal@gmail.com>
2016-03-07 03:00:21 +0100

    Added action to load associated indicator

--- web/js/custom/ui_tricks.js
+++ web/js/custom/ui_tricks.js
@@ -1,6 +1,7 @@
 $(function() {
     $("#indicatorDefinition").hide();
     $("#addIndicator").click(function() {
+        addNewIndicator();
         $("#indicatorDefinition").show();
         $('body').animate({
             scrollTop: $("#indicatorDefinition").offset().top
@@ -13,3 +14,31 @@ $(function() {
         }, 2000);
     });
 });
+
+function loadIndicator(indicatorName){
+    $(function() {
+        $.ajax({
+            type: "GET",
+            url: "/indicators/loadIndFromQnSetToEditor",
+            data: {indName: $(indicatorName).attr("id")},
+            dataType: "json",
+            success: function (response) {
+                if (response == null) {
+                    $("#indDeleteDialog").html("The selected Indicator cannot be loaded into the editor as it a composite Indicator or a NULL value.");
+                    refreshQuestionSummary();
+                    $('.indDeleteDialog').dialog('option', 'title', 'Indicator Load Message');
+                    $("#indDeleteDialog").dialog("open");
+                }
+                else {
+                    $("#indDeleteDialog").html("The selected Indicator has been successfully loaded into the Editor.<br/> Please note that it has been <strong>deleted </strong>" +
+                        "from the Question. So after making changes, please save it again if you want it to be associated with the Question. <br/>" +
+                        "Also note that you have to select again Platform and Action to populate the List of Categories.");
+                    $('.indDeleteDialog').dialog('option', 'title', 'Indicator Load Message');
+                    refreshQuestionSummary();
+                    updateScreenAfterLoadInd(response);
+                    $("#indDeleteDialog").dialog("open");
+                }
+            }
+        });
+    });
+}
